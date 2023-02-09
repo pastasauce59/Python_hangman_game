11910ba560f9f2861f4e65988b8f3e14156f8e04
Michael Primavera <pastasauce59@gmail.com>
2023-02-09 15:10:43 -0500

    initial commit

---
+++ practice_hangman.py
@@ -0,0 +1,26 @@
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+import random
+
+rand_num = random.randint(0,len(word_list) - 1)
+
+#1
+chosen_word = word_list[rand_num]
+
+#2
+guess = input("Guess a letter in the word. ").lower()
+
+#3
+for letter in chosen_word:
+    if letter == guess:
+        print("correct")
+    else:
+        print("WRONG")
